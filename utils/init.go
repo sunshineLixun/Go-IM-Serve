3665d25f6fa957fa27b0b9ade019b6868d5dbb85
Hello Li <1261142605@qq.com>
2023-03-28 14:21:50 +0800

    feat: add SQL Logger

--- utils/init.go
+++ utils/init.go
@@ -5,6 +5,10 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 var db *gorm.DB
@@ -20,7 +24,19 @@ func InitConfig() {
 }
 
 func InitMySQL() {
-	_db, err := gorm.Open(mysql.Open(viper.GetString("mysql.dsn")), &gorm.Config{})
+
+	// add log
+
+	newLogger := logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+		SlowThreshold:             time.Second, // 慢 SQL 阈值
+		LogLevel:                  logger.Info, // 日志级别
+		IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+		Colorful:                  true,        // 禁用彩色打印
+	})
+
+	_db, err := gorm.Open(mysql.Open(viper.GetString("mysql.dsn")), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		panic("failed to connect database")
 	}
